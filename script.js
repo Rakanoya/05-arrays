91d6668b037d9c8af4ab26fa40f81daee2df0c60
Kaitlyn Dennis <156827880+Rakanoya@users.noreply.github.com>
2025-05-11 17:53:09 +0000

    movie list arrays

--- script.js
+++ script.js
@@ -3,17 +3,36 @@ document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
 
-  // Add movies to the movieList based on the selected genre
+  // Create a variable to hold the list of movies for the selected genre
+  let movieList = [];
+
+  // Add movie titles directly to movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    console.log("You selected 'Comedy'");
+    // Comedy movies
+    movieList.push("The Mask");
+    movieList.push("Superbad");
+    movieList.push("Dumb and Dumber");
+    movieList.push("Home Alone");
   } else if (selectedGenre === "Action") {
-    console.log("You selected 'Action'");
+    // Action movies
+    movieList.push("Die Hard");
+    movieList.push("Mad Max: Fury Road");
+    movieList.push("John Wick");
+    movieList.push("The Avengers");
   } else if (selectedGenre === "Drama") {
-    console.log("You selected 'Drama'");
+    // Drama movies
+    movieList.push("Forrest Gump");
+    movieList.push("The Shawshank Redemption");
+    movieList.push("The Godfather");
+    movieList.push("A Beautiful Mind");
   } else if (selectedGenre === "Sci-Fi") {
-    console.log("You selected 'Sci-Fi'");
+    // Sci-Fi movies
+    movieList.push("Inception");
+    movieList.push("The Matrix");
+    movieList.push("Interstellar");
+    movieList.push("Blade Runner");
   }
 
   // Display the list of movies on the page
-  document.getElementById("movieRecommendations").innerText = `Enjoy: !`;
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}`;
 });
